1f3d74cc0387fbc9e9695aacdf9f804b8d190270
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-02-16 17:30:44 +0900

    초기 환경세팅

---
+++ models/product.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
